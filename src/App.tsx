1b98a56168016796ebdf8c2bf55818fb1ff3c889
root <root@LAPTOP-87G7INA5.>
2024-12-22 19:47:07 +0000

    updated workflows

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>This is a static website created for DA-1</h1>
+      <h1>This is a static website created for DA_1</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
